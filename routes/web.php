fdb2303e0860c922e5184191954d0698a7f1e0ba
jfm <workjfm142837@163.com>
2020-08-29 14:14:29 +0800

    first committ

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+
+Auth::routes(['verify' => true]);
+
+// auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    // 收货地址
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+    Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+    Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+    Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+});
+
