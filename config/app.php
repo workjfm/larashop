8d266f0eb0725ccd80fe1fb6d74e3bce45ae48c8
jfm <workjfm142837@163.com>
2020-08-29 15:49:57 +0800

    关闭未支付订单

--- config/app.php
+++ config/app.php
@@ -56,6 +56,8 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
+    'order_ttl' => 1800,
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
