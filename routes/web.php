cd1c04d9a100f5fc986e19808562f63efedff1bb
jfm <workjfm142837@163.com>
2020-08-29 14:52:43 +0800

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Auth::routes(['verify' => true]);
 
 // 商品
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
@@ -23,5 +23,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+
 });
 
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
